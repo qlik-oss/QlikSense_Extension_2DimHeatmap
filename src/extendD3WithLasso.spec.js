96f6418f972ff2235b21cb1315096ae3cb8b6e32
Albert Backenhof <EXT_BPD@qlik.com>
2019-03-27 10:53:55 +0100

    Aligned build to Dashboard bundle extension builds
    -Part of the work to streamline how the extensions
    are handled, irregardless of what bundle.
    
    Issue: DEB-130, DEB-133

--- src/extendD3WithLasso.spec.js
+++
@@ -1,8 +0,0 @@
-import * as d3 from 'd3';
-import './extendD3WithLasso';
-
-describe('extendD3WithLasso', () => {
-  it('should extend d3 with lasso', () => {
-    expect(d3.lasso).toBeDefined();
-  });
-});
